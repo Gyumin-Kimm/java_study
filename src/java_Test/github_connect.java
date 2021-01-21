a6cc64c23a563c704027b4df59c6c4335d0bab5b
Gyumin Kim <75244920+sinpodong@users.noreply.github.com>
2021-01-21 16:28:40 +0900

    2nd try

--- src/java_Test/github_connect.java
+++ src/java_Test/github_connect.java
@@ -5,6 +5,7 @@ public class github_connect {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("12345");
+		System.out.println("67890");
 	}
 
 }
