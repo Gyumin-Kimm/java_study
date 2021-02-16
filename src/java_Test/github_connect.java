f3e9f482f3f012066c542039f3b8823231514d23
Gyumin Kim <75244920+sinpodong@users.noreply.github.com>
2021-02-16 18:15:02 +0900

    if else Test

--- src/java_Test/github_connect.java
+++ src/java_Test/github_connect.java
@@ -5,25 +5,30 @@ import java.util.Scanner;
 public class github_connect {
 
 	public static void main(String[] args) {
-		boolean isLoop = true;
 		Scanner scanner = new Scanner(System.in);
-		
-		while(isLoop) {
-			System.out.print("input the value : ");
 
-			String val = scanner.nextLine();
-			System.out.println(val);
-			
-			System.out.print("input the value2 : ");
-			String val2 = scanner.nextLine();
-			System.out.println(val2);
-			
-			if(val.equals("q")) {
-				isLoop = false;
-			}
+		int val = scanner.nextInt();
+		char grade;
+
+		if (val > 100) {
+			System.out.println("input error");
+			return;
 		}
-		
-		
+
+		if (val >= 90) {
+			grade = 'A';
+		} else if (val >= 80) {
+			grade = 'B';
+		} else if (val >= 70) {
+			grade = 'C';
+		} else if (val >= 60) {
+			grade = 'D';
+		} else {
+			grade = 'F';
+		}
+
+		System.out.println("score : " + val + " grade : " + grade);
+
 	}
 
 }
