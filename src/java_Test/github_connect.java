5fe75f32c9f5f860d7c28d3826a9b7cc38e0fd89
Gyumin Kim <75244920+sinpodong@users.noreply.github.com>
2021-02-16 17:35:18 +0900

    upload test

--- src/java_Test/github_connect.java
+++ src/java_Test/github_connect.java
@@ -1,11 +1,29 @@
 package java_Test;
 
+import java.util.Scanner;
+
 public class github_connect {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("12345");
-		System.out.println("67890");
+		boolean isLoop = true;
+		Scanner scanner = new Scanner(System.in);
+		
+		while(isLoop) {
+			System.out.print("input the value : ");
+
+			String val = scanner.nextLine();
+			System.out.println(val);
+			
+			System.out.print("input the value2 : ");
+			String val2 = scanner.nextLine();
+			System.out.println(val2);
+			
+			if(val.equals("q")) {
+				isLoop = false;
+			}
+		}
+		
+		
 	}
 
 }
