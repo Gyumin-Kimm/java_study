15e8f6f93f480f9547467f4c94cf94d10f358035
Gyumin Kim <75244920+sinpodong@users.noreply.github.com>
2021-02-21 12:43:13 +0900

    lecture practice

--- src/codingtest_practice/excute.java
+++ src/codingtest_practice/excute.java
@@ -5,61 +5,69 @@ import java.util.Arrays;
 public class excute {
 
 	public static void main(String[] args) {
-		int[] gt1 = { 1, 3, 2, 4, 2 };
-//		int[] gt2 = { 1, 2, 3, 4, 5 };
-//		int[] gt3 = { 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1,1,1 };
-		guessTest gt = new guessTest();
-		System.out.println(Arrays.toString(gt.solution(gt1)));
-//		System.out.println(Arrays.toString(gt.solution(gt2)));
-//		System.out.println(Arrays.toString(gt.solution(gt3)));
-		System.out.println("-----------------------");
-
-		centerStr cs = new centerStr();
-//		System.out.println(cs.solution("abcde"));
-		System.out.println(cs.solution("qwer"));
-		System.out.println("-----------------------");
-
-		int[] btc1 = { 2, 3, 4 };
-		int[] btc2 = { 1, 3, 5 };
-		borrowTraningClothes btc = new borrowTraningClothes();
-		System.out.println(btc.solution(5, btc1, btc2));
-		System.out.println("-----------------------");
-
-		reverseThree rt = new reverseThree();
-		System.out.println(rt.solution(125));
-		System.out.println("-----------------------");
-
-		int[] a1 = { 2, 1, 3, 4, 1 };
-//		int[] a1 = {5, 0, 2, 7};
-		sumListTwoNum sl = new sumListTwoNum();
-		System.out.println(Arrays.toString(sl.solution(a1)));
-		System.out.println("-----------------------");
-
-//		int[][] c = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
-//		int[] d = {1, 5, 3, 5, 1, 2, 1, 4};
-		int[][] c = { { 0, 2, 0 }, { 1, 2, 0 }, { 2, 2, 1 } };
-		int[] d = { 1, 2, 2, 2, 1, 3 };
-		pickDoll pd = new pickDoll();
-		System.out.println(pd.solution(c, d));
-		System.out.println("-----------------------");
-
-		int[] a = { 1, 5, 2, 6, 3, 7, 4 };
-		int[][] b = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
-		numberKth nk = new numberKth();
-		System.out.println(Arrays.toString(nk.solution(a, b)));
-		System.out.println("-----------------------");
-
-		dayoftheweek dw = new dayoftheweek();
-		System.out.println(dw.solution(5, 24));
-		System.out.println("-----------------------");
-
-//		String[] p = {"leo", "kiki", "eden"};
-//		String[] q = {"eden", "kiki"};
-		String[] p = { "mislav", "stanko", "mislav", "ana" };
-		String[] q = { "stanko", "ana", "mislav" };
-		failRunner fr = new failRunner();
-		System.out.println(fr.solution(p, q));
-		System.out.println("-----------------------");
+//		String[] gt1 = {"Cardiology", "Orthopaedics", "Neurology", "Cardiology", "Orthopaedics", "Cardiology"};
+//		int[] gt1 = {0, 1, 1, 1, 0, 1, 1, 1, 0, 1};
+		
+		temp gt = new temp();
+//		System.out.println(temp.solution(gt1));
+		System.out.println(temp.solution(1,8,3,2));
+		System.out.println(temp.solution(2,3,3,2));
+		
+//		int[] gt1 = { 1, 3, 2, 4, 2 };
+////		int[] gt2 = { 1, 2, 3, 4, 5 };
+////		int[] gt3 = { 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1,1,1,1 };
+//		guessTest gt = new guessTest();
+//		System.out.println(Arrays.toString(gt.solution(gt1)));
+////		System.out.println(Arrays.toString(gt.solution(gt2)));
+////		System.out.println(Arrays.toString(gt.solution(gt3)));
+//		System.out.println("-----------------------");
+//
+//		centerStr cs = new centerStr();
+////		System.out.println(cs.solution("abcde"));
+//		System.out.println(cs.solution("qwer"));
+//		System.out.println("-----------------------");
+//
+//		int[] btc1 = { 2, 3, 4 };
+//		int[] btc2 = { 1, 3, 5 };
+//		borrowTraningClothes btc = new borrowTraningClothes();
+//		System.out.println(btc.solution(5, btc1, btc2));
+//		System.out.println("-----------------------");
+//
+//		reverseThree rt = new reverseThree();
+//		System.out.println(rt.solution(125));
+//		System.out.println("-----------------------");
+//
+//		int[] a1 = { 2, 1, 3, 4, 1 };
+////		int[] a1 = {5, 0, 2, 7};
+//		sumListTwoNum sl = new sumListTwoNum();
+//		System.out.println(Arrays.toString(sl.solution(a1)));
+//		System.out.println("-----------------------");
+//
+////		int[][] c = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
+////		int[] d = {1, 5, 3, 5, 1, 2, 1, 4};
+//		int[][] c = { { 0, 2, 0 }, { 1, 2, 0 }, { 2, 2, 1 } };
+//		int[] d = { 1, 2, 2, 2, 1, 3 };
+//		pickDoll pd = new pickDoll();
+//		System.out.println(pd.solution(c, d));
+//		System.out.println("-----------------------");
+//
+//		int[] a = { 1, 5, 2, 6, 3, 7, 4 };
+//		int[][] b = { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } };
+//		numberKth nk = new numberKth();
+//		System.out.println(Arrays.toString(nk.solution(a, b)));
+//		System.out.println("-----------------------");
+//
+//		dayoftheweek dw = new dayoftheweek();
+//		System.out.println(dw.solution(5, 24));
+//		System.out.println("-----------------------");
+//
+////		String[] p = {"leo", "kiki", "eden"};
+////		String[] q = {"eden", "kiki"};
+//		String[] p = { "mislav", "stanko", "mislav", "ana" };
+//		String[] q = { "stanko", "ana", "mislav" };
+//		failRunner fr = new failRunner();
+//		System.out.println(fr.solution(p, q));
+//		System.out.println("-----------------------");
 	}
 
 }
--- src/java_Test/github_connect.java
+++ src/java_Test/github_connect.java
@@ -5,42 +5,52 @@ import java.util.Scanner;
 public class github_connect {
 
 	public static void main(String[] args) {
-		int day = 0;
-		int month;
+
+		char op = '/';
+		int num1 = 10;
+		int num2 = 5;
 		
-		Scanner scanner = new Scanner(System.in);
-		
-		month = scanner.nextInt();
-		
-		switch(month) {
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			day = 31;
+		//switch
+		switch(op) {
+		case '+':
+			System.out.println(num1+num2);
 			break;
-			
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			day = 30;
+		case '-':
+			System.out.println(num1-num2);
 			break;
-			
-		case 2:
-			day = 28;
+		case '*':
+			System.out.println(num1*num2);
+			break;
+		case '/':
+			if(num2 == 0) {
+				System.out.println("cannot divide zero");
+			}
+			else {
+				System.out.println(num1/num2);
+			}
 			break;
 		default:
-			System.out.println("input month Error");
-			day = 0;
 			break;
 		}
 		
-		System.out.println(day);
-
+//		//if-else
+		if(op == '+') {
+			System.out.println(num1+num2);
+		}
+		else if(op == '-') {
+			System.out.println(num1-num2);
+		}
+		else if(op == '*') {
+			System.out.println(num1*num2);
+		}
+		else if(op == '/') {
+			if(num2 == 0) {
+				System.out.println("cannot divide zero");
+			}
+			else {
+				System.out.println(num1/num2);
+			}
+		}
 	}
 
 }
